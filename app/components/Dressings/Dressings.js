044466e6a79163d4b0e8d80ebeec57f6c480a3f0
Murat Sert <muratsert1453@gmail.com>
2017-10-18 23:08:05 +0100

    An initial form construct for Dressing options

--- app/components/Dressings/Dressings.js
+++ app/components/Dressings/Dressings.js
@@ -1,16 +1,100 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import { Text } from 'react-native'
+import Header from '../../layout/Header/Header'
+import {Container, Content, Form, Input, Label, Picker, Item } from 'native-base'
+const PickerItem = Picker.Item
 
 class Dressings extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      salad: 'No Salad',
+      sauce: 'No Sauce',
+      bread: 'Pitta Bread'
+    }
+  }
+
+  setSalad (selectedSalad) {
+    this.setState({salad: selectedSalad})
+  }
+
+  setSauce (selectedSauce) {
+    this.setState({sauce: selectedSauce})
+  }
+
+  setBread (selectedBread) {
+    this.setState({bread: selectedBread})
+  }
+
+  checkAndDisplayBreadSelection () {
+    if (true) {
+      return (
+        <Item fixedLabel>
+          <Label>Bread Choice</Label>
+          <Picker
+            mode="dropdown"
+            placeholder="Select Sauce"
+            selectedValue={this.state.bread}
+            onValueChange={this.setBread.bind(this)}
+          >
+            <Item label="Pitta Bread" value="Pitta Bread"/>
+            <Item label="Tortilla Wrap" value="Tortilla Wrap"/>
+            <Item label="Seperate Bread" value="Seperate Bread"/>
+            <Item label="No Bread" value="No Bread"/>
+          </Picker>
+        </Item>
+      )
+    }
+  }
+
   render () {
     return (
-      <View>
-        <Text>Salad</Text>
-        <Text>Sauce</Text>
-        <Text>Bread</Text>
-      </View>
+      <Container>
+        <Header pageTitle="Options"/>
+        <Content>
+          <Form>
+            <Form>
+              <Item fixedLabel>
+                <Label>Salad Choice</Label>
+                <Picker
+                  mode="dropdown"
+                  placeholder="Select Salad"
+                  selectedValue={this.state.salad}
+                  onValueChange={this.setSalad.bind(this)}
+                >
+                  <PickerItem label="No Salad" value="No Salad" />
+                  <PickerItem label="All Salad" value="All Salad" />
+                  <PickerItem label="Front Four" value="Front Four" />
+                </Picker>
+              </Item>
+              <Item fixedLabel>
+                <Label>Sauce Choice</Label>
+                <Picker
+                  mode="dropdown"
+                  placeholder="Select Sauce"
+                  selectedValue={this.state.sauce}
+                  onValueChange={this.setSauce.bind(this)}
+                >
+                  <PickerItem label="No Sauce" value="No Sauce" />
+                  <PickerItem label="Chilli" value="Chilli" />
+                  <PickerItem label="Garlic Mayo" value="Garlic Mayo" />
+                  <PickerItem label="Chilli + Garlic" value="Chilli + Garlic" />
+                  <PickerItem label="Chilli + Mayo" value="Chilli + Mayo" />
+                  <PickerItem label="BBQ" value="BBQ" />
+                  <PickerItem label="Ketchup" value="Ketchup" />
+                  <PickerItem label="Mint" value="Mint" />
+                  <PickerItem label="Burger Sauce" value="Burger Sauce" />
+                </Picker>
+              </Item>
+              {
+                this.checkAndDisplayBreadSelection()
+              }
+            </Form>
+          </Form>
+        </Content>
+      </Container>
     )
   }
 }
 
-export default Dressings
+module.exports = Dressings
