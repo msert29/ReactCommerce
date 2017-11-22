705b4459c51a01701302f3461e3396db963174e7
Murat Sert <muratsert1453@gmail.com>
2017-11-22 22:03:31 +0000

    Segragation of style from code

--- app/components/ImageNav/ImageNavStyles.js
+++ app/components/ImageNav/ImageNavStyles.js
@@ -1,15 +1,35 @@
-import React, {Component} from 'react';
-import {StyleSheet, Dimensions} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Dimensions } from 'react-native';
 const width = Dimensions.get('window').width;
 
 module.exports = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: width,
-        height: 50,
-    },
-    image: {
-        width: width,
-        height: 20,
-    }
+  container: {
+    flex: 1,
+    width: width,
+    height: 50,
+  },
+  image: {
+    flex: 1,
+    width: width,
+    marginBottom: 1,
+  },
+  container: {
+    display: 'flex',
+    flex: 3,
+    width: width,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  textContainer: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    backgroundColor: "transparent",
+    fontWeight: "100",
+    fontSize: 50,
+    color: "white"
+  }
 });
